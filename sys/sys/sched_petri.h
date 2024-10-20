f9a8d419ae30ed996374a7eaf247acc7300e12e7
nicolasgoldman07 <nicolasgoldman07@gmail.com>
2024-10-20 20:12:27 -0300

    feat(cpu-on-off): add CPU toggle ON-OFF module using Petri-net
    This commit introduces a new CPU ON-OFF toggle module, enabling the activation and deactivation of CPU queuing based on user input. The module is implemented using a Petri-net model, leveraging modifications to the incidence and inhibition matrices.
    
    The key feature of this module is the method that allows users to toggle CPU queuing on or off. This provides greater control over CPU resource management, allowing users to pause or resume CPU scheduling as needed. The integration of Petri-net ensures that state transitions are handled formally, maintaining system consistency.

--- sys/sys/sched_petri.h
+++ sys/sys/sched_petri.h
@@ -6,8 +6,8 @@
 
 #define CPU_NUMBER 4
 // FOR GLOBAL TRANISTIONS
-#define CPU_BASE_PLACES 5
-#define CPU_BASE_TRANSITIONS 9
+#define CPU_BASE_PLACES 6
+#define CPU_BASE_TRANSITIONS 12
 #define CPU_NUMBER_PLACES (CPU_BASE_PLACES*CPU_NUMBER)+3
 #define CPU_NUMBER_TRANSITION (CPU_BASE_TRANSITIONS*CPU_NUMBER)+4
 /* Definitions of transition and places for the CPU resource net */
@@ -17,6 +17,7 @@
 #define PLACE_CPU 2
 #define PLACE_TOEXEC 3
 #define PLACE_EXECUTING 4
+#define PLACE_SUSPENDED 5
 
 
 // Global queue independent of the number of CPUs
@@ -29,12 +30,14 @@
 #define TRAN_UNQUEUE 1
 #define TRAN_EXEC 2
 #define TRAN_EXEC_EMPTY 3
-#define TRAN_RETURN_VOL 4
-#define TRAN_RETURN_INVOL 5
-#define TRAN_FROM_GLOBAL_CPU 6
-#define TRAN_REMOVE_QUEUE 7
-#define TRAN_REMOVE_EMPTY_QUEUE 8
-
+#define TRAN_EXEC_IDLE 4
+#define TRAN_RETURN_VOL 5
+#define TRAN_RETURN_INVOL 6
+#define TRAN_FROM_GLOBAL_CPU 7
+#define TRAN_REMOVE_QUEUE 8
+#define TRAN_REMOVE_EMPTY_QUEUE 9
+#define TRAN_SUSPEND_PROC 10
+#define TRAN_WAKEUP_PROC 11
 
 //Global transition
 #define TRAN_REMOVE_GLOBAL_QUEUE (CPU_NUMBER_TRANSITION-4) 
@@ -70,5 +73,7 @@ void resource_execute_thread(struct thread *newtd, int cpu);
 void resource_remove_thread(struct thread *newtd, int cpu);
 void print_detailed_places(void);
 void set_print_transition(int transitions_to_print);
+void toggle_active_cpu(int cpu);
+int get_place_tokens_qty(int place_index);
 
 #endif
